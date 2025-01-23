ba700621123f6147ef84382b47fb5165dacfd83d
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-01-23 15:06:06 +0600

    change some codes in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
 import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
-
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+// Async function to fetch user session
 const Navbar = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
   return (
-    <div className=" bg-gray-200 w-full">
-      <div className=" flex items-center justify-between px-10 py-6">
+    <div className="bg-gray-200 w-full">
+      <div className="flex items-center justify-between px-10 py-6">
         <Link href={"/"} className="flex items-center gap-1">
-          <Image
-            src={"/logo.png"}
-            width={50}
-            height={50}
-            alt="MindCrest"
-          ></Image>
+          <Image src={"/logo.png"} width={50} height={50} alt="MindCrest" />
           <h1 className="font-bold text-4xl">MindCrest</h1>
         </Link>
         <div>
@@ -26,12 +22,12 @@ const Navbar = async () => {
             {user ? (
               <>
                 <Link href={"/profile"}>Profile</Link>
-                <LogoutLink className="border bg-red-100 text-red-600 border-red-200 px-2 py-1 rounded-lg ">
+                <LogoutLink className="border bg-red-100 text-red-600 border-red-200 px-2 py-1 rounded-lg">
                   Log out
                 </LogoutLink>
               </>
             ) : (
-              <LoginLink className="border bg-green-100 text-green-600 border-green-200 px-2 py-1 rounded-lg ">
+              <LoginLink className="border bg-green-100 text-green-600 border-green-200 px-2 py-1 rounded-lg">
                 Login
               </LoginLink>
             )}
