94ac055b6733ea591d8b4d4d2a8d98057a97fac4
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-01-23 11:11:03 +0600

    added website name & all routes in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="border-2 border-red-600 w-full p-8">
-      <div>
-        <div>
+    <div className=" bg-gray-200 w-full">
+      <div className=" flex items-center justify-between px-10 py-6">
+        <div className="flex items-center gap-1">
           <Image
             src={"/logo.png"}
             width={50}
             height={50}
             alt="MindCrest"
           ></Image>
+          <h1 className="font-bold text-4xl">MindCrest</h1>
+        </div>
+        <div>
+          <ul className="flex items-center gap-10 font-semibold text-base">
+            <Link href={"/"}>Home</Link>
+            <Link href={"/profile"}>Profile</Link>
+            <Link href={"/login"}>Login</Link>
+          </ul>
         </div>
       </div>
     </div>
