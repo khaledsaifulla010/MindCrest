30cedf061fa492e8c39908e06bd0031815f64af2
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-01-23 12:58:29 +0600

    added profile page

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import AllPosts from "@/components/AllPosts";
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
       <div>
         <AllPosts />
       </div>
---
+++ src/app/profile/page.jsx
@@ -0,0 +1,11 @@
+const page = () => {
+  return (
+    <div>
+      <h1 className="mt-56 text-center font-bold text-5xl">
+        Welcome to your profile!
+      </h1>
+    </div>
+  );
+};
+
+export default page;
