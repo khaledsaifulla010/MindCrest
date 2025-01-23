d28b96fb8afa1182f652c91b5056b6d6b554f6de
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-01-23 12:23:52 +0600

    show all posts using card and connect with the link

--- src/components/LoadAllPosts.jsx
+++ src/components/LoadAllPosts.jsx
@@ -14,7 +14,7 @@ const LoadAllPosts = () => {
       const data = await fetch("https://jsonplaceholder.typicode.com/posts");
       const posts = await data.json();
       setPosts(posts);
-      setVisiblePosts(posts.slice(0, 6));
+      setVisiblePosts(posts.slice(0, 12));
     };
 
     fetchPosts();
@@ -22,7 +22,7 @@ const LoadAllPosts = () => {
 
   const handleTogglePosts = () => {
     if (showAll) {
-      setVisiblePosts(posts.slice(0, 6));
+      setVisiblePosts(posts.slice(0, 12));
     } else {
       setVisiblePosts(posts);
     }
@@ -31,12 +31,12 @@ const LoadAllPosts = () => {
 
   return (
     <div>
-      <div className="grid grid-cols-3 gap-y-24 ml-24 mt-12">
+      <div className="grid grid-cols-4 gap-y-12 ml-6 mt-12">
         {visiblePosts.map((post) => (
           <PostCard key={post.id} post={post}></PostCard>
         ))}
       </div>
-      <div className="ml-[680px] mt-24 mb-12">
+      <div className="ml-[680px] mt-12 mb-12">
         <ShowAllButton
           onToggle={handleTogglePosts}
           showAll={showAll}
--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,13 +1,22 @@
-import { Card, CardContent } from "./ui/card";
+import Link from "next/link";
+import { Card, CardHeader } from "./ui/card";
 
 const PostCard = ({ post }) => {
+  const truncatedTitle =
+    post.title.split(" ").slice(0, 4).join(" ") +
+    (post.title.split(" ").length > 4 ? "..." : "");
   return (
     <div>
-      <Card className="w-[350px]">
-        <CardContent>
-          <h1>Header {post.id} </h1>
-        </CardContent>
-      </Card>
+      <Link href={`/`}>
+        <Card className="w-[300px] h-[100px] border border-blue-100 transition-all duration-1000 hover:shadow-lg hover:scale-105">
+          <CardHeader>
+            <h1 className="text-2xl text-center font-bold">
+              {" "}
+              {truncatedTitle}{" "}
+            </h1>
+          </CardHeader>
+        </Card>
+      </Link>
     </div>
   );
 };
