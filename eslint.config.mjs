9a2b254b08bac7150fca650ada3f36dc8add591a
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-01-23 14:41:48 +0600

    fixes some .env related problems

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,21 @@
-// eslint.config.mjs
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const compat = new FlatCompat();
 
-// Using FlatCompat for compatibility with ESLint configuration extending
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-// Define the ESLint config
-const eslintConfig = {
-  overrides: [
-    {
-      files: ["*.js", "*.mjs", "*.ts", "*.tsx"], // Targeting JS and TS files
-      extends: [
-        "eslint:recommended",
-        "plugin:@next/next/core-web-vitals", // Next.js specific config
-        ...compat.extends("next/core-web-vitals"),
-      ],
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: "module",
-      },
-      rules: {
-        // Custom rules here
-      },
+const eslintConfig = [
+  compat.extends("eslint:recommended"),
+  compat.extends("plugin:@next/next/core-web-vitals"),
+  {
+    // Custom configuration rules
+    files: ["*.js", "*.mjs", "*.ts", "*.tsx"],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
     },
-  ],
-};
+    rules: {
+      // Custom rules here
+    },
+  },
+];
 
 export default eslintConfig;
