83cd6283f44fc8973b4b7a2d907eae3c92226b94
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-01-23 14:31:21 +0600

    fixes some .env related problems

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    KINDE_CLIENT_ID: process.env.KINDE_CLIENT_ID,
+    KINDE_CLIENT_SECRET: process.env.KINDE_CLIENT_SECRET,
+    KINDE_ISSUER_URL: process.env.KINDE_ISSUER_URL,
+    KINDE_SITE_URL: process.env.KINDE_SITE_URL,
+    KINDE_POST_LOGOUT_REDIRECT_URL: process.env.KINDE_POST_LOGOUT_REDIRECT_URL,
+    KINDE_POST_LOGIN_REDIRECT_URL: process.env.KINDE_POST_LOGIN_REDIRECT_URL,
+  },
+};
 
 export default nextConfig;
